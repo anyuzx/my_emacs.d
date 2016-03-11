03da1f9eb45dd481892d147790c6710c5094b8fa
anyuzx <stefanshi1988@gmail.com>
2016-03-10 22:46:52 -0600

    First commit emacs.d

---
+++ lisp/init-locales.el
@@ -0,0 +1,27 @@
+(defun sanityinc/utf8-locale-p (v)
+  "Return whether locale string V relates to a UTF-8 locale."
+  (and v (string-match "UTF-8" v)))
+
+(defun sanityinc/locale-is-utf8-p ()
+  "Return t iff the \"locale\" command or environment variables prefer UTF-8."
+  (or (sanityinc/utf8-locale-p (and (executable-find "locale") (shell-command-to-string "locale")))
+      (sanityinc/utf8-locale-p (getenv "LC_ALL"))
+      (sanityinc/utf8-locale-p (getenv "LC_CTYPE"))
+      (sanityinc/utf8-locale-p (getenv "LANG"))))
+
+(when (or window-system (sanityinc/locale-is-utf8-p))
+  (set-language-environment 'utf-8)
+  (setq locale-coding-system 'utf-8)
+  (set-default-coding-systems 'utf-8)
+  (set-terminal-coding-system 'utf-8)
+  (set-selection-coding-system (if (eq system-type 'windows-nt) 'utf-16-le 'utf-8))
+  (prefer-coding-system 'utf-8))
+
+(defun my/markdown-preview-file ()
+  "use Marked 2 to preview the current file"
+  (interactive)
+  (shell-command
+   (format "open -a 'Marked 2.app' %s"
+           (shell-quote-argument (buffer-file-name)))))
+(global-set-key "\C-cm" 'my/markdown-preview-file)
+(provide 'init-locales)
---
+++ org-clock-save.el
@@ -0,0 +1,1 @@
+;; org-persist.el - wel-145-31.cm.utexas.edu at <2016-03-07 Mon 14:15>
